074487fffc857926e8aa757e1850ad157ba50f19
lPinchol <pinchodev91@gmail.com>
2016-12-21 14:36:13 +0100

    Update project

---
+++ X Project/Assets/Proyecto M/Scripts/Clases/clsSprite.cs
@@ -0,0 +1,21 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//                                                                              \\
+// clsSprite.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEngine.UI;
+#endregion
+
+public class clsSprite : MonoBehaviour 
+{
+    public int id;
+    public string nombre;
+    public bool arrastable;
+    public GameObject prefab;
+}
---
+++ X Project/Assets/Proyecto M/Scripts/Core/MSprite.cs
@@ -0,0 +1,27 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//                                                                              \\
+// MSprite.cs (00/00/0000)													    \\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections.Generic;
+
+#endregion
+
+public class MSprite : MonoBehaviour 
+{
+    public List<clsSprite> elementos = new List<clsSprite>();
+
+    public void Start()
+    {
+        for (int i = 0; i < elementos.Count; i++)
+        {
+            elementos[i].prefab.name = elementos[i].nombre;
+        }
+    }
+}
